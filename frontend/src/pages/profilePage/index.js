e4c4beaed15bac213785858da3a090861af2600b
ShawLue <69040079+ShawLue@users.noreply.github.com>
2021-10-08 21:12:59 -0400

    profile page added

---
+++ frontend/src/pages/profilePage/index.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+// image, name, introduciton, age, 
+
+function profile() {
+    const profile = (
+        <div id="root">
+            <div id="image">
+                <img src="" width="200"></img>
+            </div>
+            <div id="name">
+                <h2>name here</h2>
+            </div>
+            <div id="age">
+                <h3>age here</h3>
+            </div>
+            <div id="introduction">
+                <p>
+                </p>
+            </div>
+        </div>
+    );
+    ReactDOM.render(landingPage, getElementByID('root'));
+}
