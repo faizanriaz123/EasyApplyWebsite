839c0fe130ea2552633aad501e5ad9e21c8216c0
ShawLue <69040079+ShawLue@users.noreply.github.com>
2021-10-28 13:13:31 -0400

    modified coloring

--- frontend/src/pages/Landing/index.js
+++ frontend/src/pages/Landing/index.js
@@ -95,23 +95,24 @@ const tiers = [
 
 function PricingContent() {
   return (
-    <React.Fragment>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+    <React.Fragment >
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }}/>
       <CssBaseline />
       <AppBar
         position="static"
         color="default"
         elevation={0}
         sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+        style={{ backgroundColor: '#2b2b2b' }}
       >
         <Toolbar sx={{ flexWrap: 'wrap' }}>
           <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
             <img src={logo} className="logo"></img>
           </Typography>
-          <Button href="/signup" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+          <Button href="/signup" variant="outlined" sx={{ my: 1, mx: 1.5 }} style={{ color: "#00FECF" }}>
             Sign up now!
           </Button>
-          <Button href="/login" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+          <Button href="/login" variant="outlined" sx={{ my: 1, mx: 1.5 }} style={{ color: "#00FECF" }}>
             Login
           </Button>
         </Toolbar>
@@ -209,7 +210,7 @@ function PricingContent() {
           py: [3, 6],
         }}
       >
-        <Copyright sx={{ mt: 5 }} />
+        <Copyright sx={{ mt: 5 }}/>
       </Container>
       {/* End footer */}
     </React.Fragment>
